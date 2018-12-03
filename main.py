2e8bf36e1a0e174dce86a3af686701b97717a11d
Sanish Maharjan <sanishmaharjan@lftechnology.com>
2018-12-03 16:46:17 +0545

    Initial code base

---
+++ main.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+"""
+Documentation
+
+See also https://www.python-boilerplate.com/flask
+"""
+import os
+
+from flask import Flask, jsonify, request, g
+from flask_cors import CORS
+from logzero import logger
+from time import time
+import traceback
+import http
+from datetime import datetime
+
+
+def create_app(config=None):
+    app = Flask(__name__)
+
+    # See http://flask.pocoo.org/docs/latest/config/
+    app.config.update(dict(DEBUG=True))
+    app.config.update(config or {})
+
+    # Setup cors headers to allow all domains
+    # https://flask-cors.readthedocs.io/en/latest/
+    CORS(app)
+
+    @app.before_request
+    def before_request():
+        """
+            A function to run before each request.
+        """
+
+        logger.debug('Request [{request_method}] : {request_url}'
+                      .format(request_method=request.method, request_url=request.base_url))
+        g.start_time = time()
+
+    @app.after_request
+    def after_request(response):
+        """
+            A function to run after each request.
+        """
+
+        execution_time = time() - g.start_time
+        logger.debug('Request completion time: {execution_time}'.format(execution_time=execution_time))
+
+        return response
+
+    @app.errorhandler(Exception)
+    def exceptions(error):
+        ts = datetime.now().strftime('[%Y-%b-%d %H:%M]')
+        tb = traceback.print_stack(limit=5)
+        path = "?".join(
+            [request.path,
+             request.query_string]) if request.query_string else request.path
+        logger.error('%s %s %s %s %s 500 INTERNAL SERVER ERROR\n%s',
+                     ts, request.remote_addr, request.method, request.scheme, path, tb)
+
+        return 'Internal Server Error', http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+    # Definition of the routes. Put them into their own file. See also
+    # Flask Blueprints: http://flask.pocoo.org/docs/latest/blueprints
+    @app.route("/")
+    def hello_world():
+        return "Boiler-Plate Python Application"
+
+    return app
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app = create_app()
+    app.run(host="0.0.0.0", port=port)
---
+++ test_flask.py
@@ -0,0 +1,30 @@
+"""
+You can auto-discover and run all tests with this command:
+
+    $ pytest
+
+Documentation:
+
+* https://docs.pytest.org/en/latest/
+* https://docs.pytest.org/en/latest/fixture.html
+* http://flask.pocoo.org/docs/latest/testing/
+"""
+
+import pytest
+
+import main
+
+
+@pytest.fixture
+def app():
+    app = main.create_app()
+    app.debug = True
+    return app.test_client()
+
+
+def test_hello_world(app):
+    res = app.get("/")
+    # print(dir(res), res.status_code)
+    assert res.status_code == 200
+    assert b"Boiler-Plate Python Application" in res.data
+
