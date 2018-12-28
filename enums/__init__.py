8aa4940f0bf289303fe04ec373e8a258ea069300
Sanish Maharjan <sanishmaharjan@lftechnology.com>
2018-12-28 17:34:42 +0545

    add enum sample

---
+++ enums/__init__.py
@@ -0,0 +1,1 @@
+from .role_enum import RoleEnum
---
+++ enums/role_enum.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class RoleEnum(Enum):
+    """
+        Role Enum.
+    """
+
+    USER = 'user'
+    ADMIN = 'admin'
+    GUEST = 'guest'
